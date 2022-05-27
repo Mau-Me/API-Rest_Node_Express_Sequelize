2766de2ce9d1092dfa7cf22dbc732e5dd12c029c
Mau-Me <mauriciosm95@gmail.com>
2022-05-27 16:07:19 -0300

    Feat: Add controller e routes de turmas

---
+++ api/routes/niveisRoute.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const NivelController = require("../controllers/NivelController");
+
+const router = Router();
+router
+  .get("/niveis", NivelController.listarNiveis)
+  .get("/niveis/:id", NivelController.buscarNivelPorId)
+  .post("/niveis", NivelController.inserirNivel)
+  .put("/niveis/:id", NivelController.atualizarNivel)
+  .delete("/niveis/:id", NivelController.apagarNivel);
+module.exports = router;
