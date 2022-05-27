b7d6be343643480dc9cad1d23dad2dcc1c30e0ea
Mau-Me <mauriciosm95@gmail.com>
2022-05-27 16:15:56 -0300

    Feat: Add ao index as rotas de niveis e turmas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const pessoasRoutes = require("./pessoasRoute");
+const niveisRoutes = require("./niveisRoute");
+const turmasRoutes = require("./turmasRoute");
+
 module.exports = (app) => {
-  app.use(express.json());
-  app.use(pessoasRoutes);
+  app.use(express.json(), pessoasRoutes, niveisRoutes, turmasRoutes);
 
   app.get("/", (req, res) => {
     res.send("Bem Vindo");
