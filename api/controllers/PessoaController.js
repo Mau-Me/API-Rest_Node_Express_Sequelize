5217835caea817b6d3f284a3f8f449bea52dc239
Mau-Me <mauriciosm95@gmail.com>
2022-05-27 02:29:07 -0300

    Feat: Add rotas de busca e buscar por id

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -7,7 +7,29 @@ class PessoaController {
       const pessoas = await database.Pessoas.findAll();
       return res.json(pessoas);
     } catch (error) {
-      res.status(500).json(error.message);
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async buscarPorId(req, res) {
+    try {
+      const { id } = req.params;
+      const pessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.json(pessoa);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async inserirPessoa(req, res) {
+    const { pessoa } = req.body;
+    try {
+      const pessoaCriada = await database.Pessoas.create(pessoa);
+      return res.json(pessoaCriada);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
   }
 }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,7 @@ const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 router.get("/pessoas", PessoaController.listarPessoas);
+router.get("/pessoas/:id", PessoaController.buscarPorId);
+router.post("/pessoas", PessoaController.inserirPessoa);
 
 module.exports = router;
