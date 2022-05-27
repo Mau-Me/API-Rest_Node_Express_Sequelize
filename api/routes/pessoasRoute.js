0218c6fffda5d44589fa9a8da65a171c676f1540
Mau-Me <mauriciosm95@gmail.com>
2022-05-27 16:14:59 -0300

    Feat: Add rotas relacionadas a matrículas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,9 +4,25 @@ const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 router.get("/pessoas", PessoaController.listarPessoas);
-router.get("/pessoas/:id", PessoaController.buscarPorId);
+router.get("/pessoas/:id", PessoaController.buscarPessoaPorId);
 router.post("/pessoas", PessoaController.inserirPessoa);
 router.put("/pessoas/:id", PessoaController.atualizarPessoa);
 router.delete("/pessoas/:id", PessoaController.apagarPessoa);
+router.get(
+  "/pessoas/:estudanteId/matricula",
+  PessoaController.buscarMatriculasPorId
+);
+router.post(
+  "/pessoas/:estudanteId/matricula",
+  PessoaController.inserirMatricula
+);
+router.put(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.atualizarMatricula
+);
+router.delete(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.apagarMatricula
+);
 
 module.exports = router;
